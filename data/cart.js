5378a6e64a3996e586a9c6b20b7553ae27e867bc
Maduagwu Martins <maduagwumartins16@gmail.com>
2025-05-23 00:05:35 +0100

    generated html for checkout page.

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,16 @@
-export const cart = [];
+export const cart = [{
+    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+    prodName: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+    priceCents: 1090,
+    quantity: 2
+}, {
+    id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    image: "images/products/intermediate-composite-basketball.jpg",
+    prodName: "Intermediate Size Basketball",
+    priceCents: 2095,
+    quantity: 1,
+}];
 
 export function addCartItems(prodId, prodPrice){
     let isProductAval;
